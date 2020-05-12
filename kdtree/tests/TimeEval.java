26091fe6dff20a5c2c76217fccf2b82bf060a882
Will Burke <wgburke@wpi.edu>
2020-05-11 22:39:38 -0400

    Adding brute force to time evaluation

--- kdtree/tests/TimeEval.java
+++ kdtree/tests/TimeEval.java
@@ -11,35 +11,33 @@ import java.util.Random;
 public class TimeEval {
 
 	public static void main(String[] args) {
-
-		// Need to run time evaluation for insert method for each data set (2^5 -> 2^18)
-		
-		// For each N:
-		// Add N nodes with randomly generated point values to K-d tree
-		// Do x trials where a point is randomized and 
 		nearestNeighborTrial();
-		
-		
 	}
 	
 	static void nearestNeighborTrial() {
 		Random rd = new Random();
 		
-		System.out.println(" N\t\tAvg\t\t\tMin\t\t\tMax");
+		System.out.println(" N\t\tAvg\t\tMin\t\tMax\t\tBruteAvg\tBruteMin\tBruteMax");
 		
 		for (int ex = 2; ex <= 18; ex++) { // Looping over exponents 2-18 but only keeping 5-18
 			
 			int NUM_TRIAL = 10000; // Number of trials
 			KDTree kd = new KDTree(); // Creating a new kd tree for every exponent
+			Point[] points = new Point[(int)Math.pow(2, ex)];
 			
 			for(long n = 1; n <= (long) Math.pow(2, ex); n++) { // Adding 2^ex random points to kd tree
 				Point p = new Point(rd.nextDouble(), rd.nextDouble());
 				kd.insert(p);
+				points[(int)n-1] = p;
 			}
 			
 			long minTime = Integer.MAX_VALUE; // Minimum time so far
 			long totalTime = 0; // Total time for average
 			long maxTime = 0;
+			
+			long bruteMinTime = Integer.MAX_VALUE;
+			long bruteTotalTime = 0;
+			long bruteMaxTime = 0;
 			
 			for (int trial = 0; trial < NUM_TRIAL; trial++) { // For 20 trials
 				
@@ -54,11 +52,31 @@ public class TimeEval {
 				if (elapsed > maxTime) {
 					maxTime = elapsed;
 				}
+				
+				long startBrute = System.nanoTime();
+				// measure time to get find smallest
+				double smallestBrute = p1.distTo(points[0]);
+				for(int x = 1; x < points.length; x++) {
+					double currentDistance = p1.distTo(points[x]);
+					if(currentDistance < smallestBrute) {
+						smallestBrute = currentDistance;
+					}
+				}
+				// Now smallestBrute is the smallest point from the array.
+				long elapsedBrute = System.nanoTime() - startBrute;
+				
+				bruteTotalTime += elapsedBrute;
+				if (elapsedBrute < bruteMinTime) { bruteMinTime = elapsedBrute; }
+				if (elapsedBrute > bruteMaxTime) {
+					bruteMaxTime = elapsedBrute;
+				}
+				
 			}
 			double avgTime = totalTime/(double)NUM_TRIAL; // Now we have min time and avg time
+			double bruteAvgTime = bruteTotalTime/NUM_TRIAL;
 			
 			// report results
-			System.out.println(String.format("%2d\t\t%f\t%f\t%f", ex, avgTime / 1000.0, minTime / 1000.0, maxTime / 1000.0));
+			System.out.println(String.format("%2d\t\t%f\t%f\t%f\t%f\t%f\t%f", ex, avgTime / 1000.0, minTime / 1000.0, maxTime / 1000.0, bruteAvgTime / 1000.0, bruteMinTime/1000.0, bruteMaxTime/1000.0));
 		}
 		
 	}
