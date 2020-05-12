adbf0caf5a926fecffd551ddd1e28bf460edcdac
mgovilla <mgovilla@gmail.com>
2020-05-11 21:30:39 -0400

    Add stopwatch capability directly in TimeEval

--- kdtree/tests/TimeEval.java
+++ kdtree/tests/TimeEval.java
@@ -3,6 +3,9 @@ package kdtree.tests;
 import kdtree.KDNode;
 import kdtree.KDTree;
 import kdtree.Point;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
 import java.util.Random;
 
 public class TimeEval {
@@ -22,39 +25,40 @@ public class TimeEval {
 	static void nearestNeighborTrial() {
 		Random rd = new Random();
 		
-		System.out.println(String.format(" N\t\tAvg\t\tMin\t\tMax"));
+		System.out.println(" N\t\tAvg\t\t\tMin\t\t\tMax");
 		
-		for (int ex = 5; ex <= 18; ex++) { // Looping over exponents 5-18
+		for (int ex = 2; ex <= 18; ex++) { // Looping over exponents 2-18 but only keeping 5-18
 			
-			int NUM_TRIAL = 1000; // Number of trials
+			int NUM_TRIAL = 10000; // Number of trials
 			KDTree kd = new KDTree(); // Creating a new kd tree for every exponent
 			
 			for(long n = 1; n <= (long) Math.pow(2, ex); n++) { // Adding 2^ex random points to kd tree
-				Point p = new Point(rd.nextDouble(),rd.nextDouble());
+				Point p = new Point(rd.nextDouble(), rd.nextDouble());
 				kd.insert(p);
 			}
 			
-			double minTime = Integer.MAX_VALUE; // Minimum time so far
-			double totalTime = 0; // Total time for average
-			double maxTime = 0;
+			long minTime = Integer.MAX_VALUE; // Minimum time so far
+			long totalTime = 0; // Total time for average
+			long maxTime = 0;
 			
 			for (int trial = 0; trial < NUM_TRIAL; trial++) { // For 20 trials
 				
-				Point p1 = new Point(rd.nextDouble(),rd.nextDouble()); // Creating a new point 
-				
-				StopwatchCPU base = new StopwatchCPU(); // Starting stopwatch
+				Point p1 = new Point(rd.nextDouble(), rd.nextDouble()); // Creating a new point
+
+				long start = System.nanoTime(); // Starting stopwatch
 				kd.nearest(p1); // Method being timed
-				double newTime = base.elapsedTime(); // Stopping stopwatch
+				long elapsed = System.nanoTime() - start; // Stopping stopwatch
 				
-				totalTime+=newTime;
-				if (newTime < minTime) { minTime = newTime; }
-				if(newTime > maxTime) {maxTime = newTime;}
-				
+				totalTime += elapsed;
+				if (elapsed < minTime) { minTime = elapsed; }
+				if (elapsed > maxTime) {
+					maxTime = elapsed;
+				}
 			}
 			double avgTime = totalTime/(double)NUM_TRIAL; // Now we have min time and avg time
 			
 			// report results
-			System.out.println(String.format("%2d\t\t%f\t%f\t%f", ex, avgTime, minTime,maxTime));
+			System.out.println(String.format("%2d\t\t%f\t%f\t%f", ex, avgTime / 1000.0, minTime / 1000.0, maxTime / 1000.0));
 		}
 		
 	}
